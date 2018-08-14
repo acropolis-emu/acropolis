f8140cc76433d1fd65e280a30207876b7f16bfb9
beardypig <beardypig@protonmail.com>
2018-08-14 14:17:48 +0200

    Add clang-format config, and reformat the existing files

--- tests/test_runner.cpp
+++ tests/test_runner.cpp
@@ -1,11 +1,10 @@
-#include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
 #include <locale.h>
 
-int main(int argc, char* argv[])
-{
-    setlocale(LC_NUMERIC, "");
-    testing::InitGoogleTest(&argc, argv);
-    testing::InitGoogleMock(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char *argv[]) {
+  setlocale(LC_NUMERIC, "");
+  testing::InitGoogleTest(&argc, argv);
+  testing::InitGoogleMock(&argc, argv);
+  return RUN_ALL_TESTS();
 }
