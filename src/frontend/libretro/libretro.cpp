8c5a4fd4164e774007d22423c7ca0bf90ed71d44
beardypig <beardypig@protonmail.com>
2018-08-14 13:51:44 +0200

    Add libretro stub

---
+++ src/frontend/libretro/libretro.cpp
@@ -0,0 +1,121 @@
+/*
+ *  Acropolis  Copyright (C) 2018 Team Acropolis
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <stdio.h>
+#include <string.h>
+#include <cstdlib>
+#include <memory>
+
+#include "libretro.h"
+#include "config.h"
+
+// Callbacks
+static retro_log_printf_t log_cb;
+static retro_video_refresh_t video_cb;
+static retro_input_poll_t input_poll_cb;
+static retro_input_state_t input_state_cb;
+static retro_environment_t environ_cb;
+static retro_audio_sample_t audio_cb;
+static retro_audio_sample_batch_t audio_batch_cb;
+
+unsigned retro_api_version(void) { return RETRO_API_VERSION; }
+
+// Cheats
+void retro_cheat_reset(void) { }
+void retro_cheat_set(unsigned index, bool enabled, const char *code) { }
+
+// Load a cartridge
+bool retro_load_game(const struct retro_game_info *info)
+{
+    return true;
+}
+
+bool retro_load_game_special(unsigned game_type, const struct retro_game_info *info, size_t num_info) { return false; }
+
+// Unload the cartridge
+void retro_unload_game(void) { }
+
+unsigned retro_get_region(void) { return RETRO_REGION_NTSC; }
+
+// libretro unused api functions
+void retro_set_controller_port_device(unsigned port, unsigned device) { }
+
+void *retro_get_memory_data(unsigned id) { return nullptr; }
+size_t retro_get_memory_size(unsigned id){ return 0; }
+
+// Serialisation methods
+size_t retro_serialize_size(void) { return 0; }
+bool retro_serialize(void *data, size_t size) { return false; }
+bool retro_unserialize(const void *data, size_t size) { return false; }
+
+// End of retrolib
+void retro_deinit(void) { }
+
+// libretro global setters
+void retro_set_environment(retro_environment_t cb)
+{
+    environ_cb = cb;
+    bool no_rom = true;
+    cb(RETRO_ENVIRONMENT_SET_SUPPORT_NO_GAME, &no_rom);
+}
+
+void retro_set_audio_sample_batch(retro_audio_sample_batch_t cb) {}
+void retro_set_video_refresh(retro_video_refresh_t cb) { video_cb = cb; }
+void retro_set_audio_sample(retro_audio_sample_t cb) { audio_cb = cb; }
+void retro_set_input_poll(retro_input_poll_t cb) { input_poll_cb = cb; }
+void retro_set_input_state(retro_input_state_t cb) { input_state_cb = cb; }
+
+void retro_init(void)
+{
+    /* set up some logging */
+    struct retro_log_callback log;
+    unsigned level = 4;
+
+    if (environ_cb(RETRO_ENVIRONMENT_GET_LOG_INTERFACE, &log))
+        log_cb = log.log;
+    else
+        log_cb = nullptr;
+
+    // the performance level is guide to frontend to give an idea of how intensive this core is to run
+    environ_cb(RETRO_ENVIRONMENT_SET_PERFORMANCE_LEVEL, &level);
+}
+
+
+/*
+ * Tell libretro about this core, it's name, version and which rom files it supports.
+ */
+void retro_get_system_info(struct retro_system_info *info)
+{
+    memset(info, 0, sizeof(*info));
+    info->library_name = ACROPOLIS_NAME.c_str();
+    info->library_version = ACROPOLIS_VERSION.c_str();
+    info->need_fullpath = false;
+    info->valid_extensions = "bin";
+}
+
+/*
+ * Tell libretro about the AV system; the fps, sound sample rate and the
+ * resolution of the display.
+ */
+void retro_get_system_av_info(struct retro_system_av_info *info) { }
+
+void retro_reset(void) { }
+
+// Test the user input and return the state of the joysticks and buttons
+void get_joystick_state(unsigned port, uint8_t &x, uint8_t &y, uint8_t &b1, uint8_t &b2, uint8_t &b3, uint8_t &b4) { }
+
+// Run a single frames with out Vectrex emulation.
+void retro_run(void) { }
